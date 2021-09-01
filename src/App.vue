ba88fdb46170e33b9693c4cc8b38e716d4927d8d
YunXinZhiCheng <867395636@qq.com>
2021-09-01 22:16:13 +0800

    5-toRef函数

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="container">
-    <div>{{ obj.name }}</div>
-    <div>{{ obj.age }}</div>
+    {{ name }}
+    <br />
     <button @click="updateName">修改数据</button>
   </div>
 </template>
 
 <script>
-// 导入reactive函数
-import { reactive } from 'vue'
-
+import { reactive, toRef } from 'vue'
 export default {
   name: 'App',
   setup() {
-    //  定义一个对象:普通数据
-    // const obj = {
-    //   name: '张三',
-    //   age: 18,
-    // }
-
-    // 定义一个对象：响应式数据
+    // 1. 响应式数据对象
     const obj = reactive({
       name: '张三',
-      age: 18,
+      age: 10,
     })
-    // 定义一个函数：修改名字
+    // 2. 模板中只需要使用name数据
+    // 注意：从响应式对象中解构出来的属性数据，不再是响应式数据, 出来的是一个普通数据
+    // const { name } = obj
+
+    const name = toRef(obj, 'name')
+    console.log(name)
+
+    // 3. 修改name数据: 属性.value
     const updateName = () => {
       console.log('updateName')
-      obj.name = '李四'
+      // toRef转换响应式数据包装成对象，value存放值的位置
+      name.value = '李四'
     }
     return {
-      obj,
+      name,
       updateName,
     }
   },
