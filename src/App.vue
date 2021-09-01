e803952fc7c97d04a82320c2299cc3fb7350151f
YunXinZhiCheng <867395636@qq.com>
2021-09-01 20:47:37 +0800

    2-setup函数

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        项目初始化
-    </div>
+  <div class="container">
+    <!-- 使用数据和函数 -->
+    <h1 @click="say">{{ msg }}</h1>
+  </div>
 </template>
-
 <script>
 export default {
-   name: 'App'
+  name: 'App',
+  // 1.组合API的起点，将来的组合API的代码，基本就在这里
+  // 2.可以理解为：在beforeCreate钩子执行前执行，组件实例创建前
+  // 3.函数中不能使用this，=>undefined
+  // 4.模板中需要使用到的数据和函数，需要在setup中返回
+  setup() {
+    console.log('setup', this) // undefined
+    // 数据
+    const msg = 'Hello Vue3'
+    // 方法
+    const say = () => {
+      console.log('Hello Vue3')
+    }
+
+    // 返回一个对象
+    return {
+      msg,
+      say,
+    }
+  },
 }
 </script>
- 
-<style>
-    
-</style>
