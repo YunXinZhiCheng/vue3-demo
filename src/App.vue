7a608c1896f75764b2562b35751583f26591749b
YunXinZhiCheng <867395636@qq.com>
2021-09-01 21:08:58 +0800

    3-Lifecycle生命周期

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div class="container">
-    <!-- 使用数据和函数 -->
-    <h1 @click="say">{{ msg }}</h1>
-  </div>
+  <div class="container">生命周期</div>
 </template>
+
 <script>
+import { onBeforeMount, onMounted } from 'vue'
+
 export default {
   name: 'App',
-  // 1.组合API的起点，将来的组合API的代码，基本就在这里
-  // 2.可以理解为：在beforeCreate钩子执行前执行，组件实例创建前
-  // 3.函数中不能使用this，=>undefined
-  // 4.模板中需要使用到的数据和函数，需要在setup中返回
   setup() {
-    console.log('setup', this) // undefined
-    // 数据
-    const msg = 'Hello Vue3'
-    // 方法
-    const say = () => {
-      console.log('Hello Vue3')
-    }
-
-    // 返回一个对象
-    return {
-      msg,
-      say,
-    }
+    // 1. DOM渲染前钩子
+    onBeforeMount(() =>
+      console.log('DOM渲染前钩子', document.querySelector('.container'))
+    )
+    // 2. DOM渲染后钩子
+    onMounted(() =>
+      console.log('DOM渲染后钩子1', document.querySelector('.container'))
+    )
+    // 3.可以定义多个相同的构造函数，以实现不同的逻辑
+    onMounted(() =>
+      console.log('DOM渲染后钩子2', document.querySelector('.container'))
+    )
   },
 }
 </script>
+
+<style></style>
