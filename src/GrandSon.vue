e7a2fd96db1bae414491316c3243beee5fd73c9c
YunXinZhiCheng <867395636@qq.com>
2021-09-02 21:10:41 +0800

    16-后代通讯

---
+++ src/GrandSon.vue
@@ -0,0 +1,26 @@
+<template>
+  <h3>后代组件接收：{{ money }}</h3>
+  <button @click="fn">消费20</button>
+</template>
+
+<script>
+import { inject } from 'vue'
+export default {
+  name: 'GrandSon',
+  setup() {
+    const money = inject('money')
+    // 后代组件：消费50，通知父组件App.vue组件，进行修改
+    // 不能自己修改数据，遵循单向数据流原则，即数据谁定义就谁修改
+    const changeMoney = inject('changeMoney')
+    const fn = () => {
+      changeMoney(20)
+    }
+    return {
+      money,
+      fn,
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ src/Son2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h2>子组件接收：{{ money }}</h2>
+    <hr />
+    <GrandSon />
+  </div>
+</template>
+
+<script>
+import GrandSon from './GrandSon.vue'
+// 导入inject函数
+import { inject } from 'vue'
+export default {
+  name: 'Son2',
+  components: {
+    GrandSon,
+  },
+  setup() {
+    //  接收祖先组件提供的组件 inject()
+    const money = inject('money')
+    return {
+      money,
+    }
+  },
+}
+</script>
+
+<style></style>
