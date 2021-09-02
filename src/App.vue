1052fba49973300593f1e572e1782a1d2d500446
YunXinZhiCheng <867395636@qq.com>
2021-09-02 19:16:29 +0800

    14-父传子

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,26 @@
 <template>
   <div class="container">
-    <!-- vue2.0获取单个元素 -->
-    <!-- 1. 通过ref属性绑定修改元素 -->
-    <!-- 2. 通过this.$refs.box获取元素 -->
-    <!-- <div class="box">我是box</div> -->
-
-    <!-- vue2.0 获取v-for遍历的多个元素 -->
-    <!-- 1. 通过ref属性绑定被遍历元素 -->
-    <!-- 2. 通过this.$refs.li 获取所有遍历元素 -->
-    <!-- <ul>
-      <li v-for="i in 4" :key="i" ref="li">{{ i }}</li>
-    </ul> -->
-
-    <!-- vue3.0 单个元素-->
-    <div ref="dom">我是box</div>
-
-    <!-- vue3.0 多个元素，被遍历的时候 -->
-    <ul>
-      <li v-for="i in 4" :key="i" :ref="setDom">第{{ i }}LI</li>
-    </ul>
+    <h1>父组件</h1>
+    <p>{{ money }}</p>
+    <hr />
+    <!-- 子组件 -->
+    <Son :money="money" />
   </div>
 </template>
 
 <script>
-import { onMounted, ref } from 'vue'
+import Son from './Son.vue'
+import { ref } from 'vue'
 export default {
   name: 'App',
+  components: {
+    Son,
+  },
+  // 父组件的数据传递给子组件
   setup() {
-    // 1.获取单个元素
-    // 先定义一个空的响应式数据，ref定义的
-    // setup中返回该数据，你想获取哪个DOM元素，在该元素上使用ref属性绑定该数据即可
-    const dom = ref(null)
-    // 打印对象
-    console.log(dom)
-    // 打印对象值
-    onMounted(() => console.log('值为:' + dom.value))
-
-    // 2.获取v-for遍历的元素
-    // 定义一个空数组，接收所有的 LI
-    const domList = []
-    // 定义一个函数，往空数组添加 push DOM
-    const setDom = (el) => {
-      // console.log('setDom')
-      console.log(el)
-      domList.push(el)
-    }
-    // 打印接收后DOM元素后的数组
-    console.log(domList)
+    const money = ref(100)
     return {
-      dom,
-      setDom,
+      money,
     }
   },
 }
---
+++ src/Son.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>子组件</h1>
+    <p>{{ money }}</p>
+  </div>
+</template>
+
+<script>
+import { onMounted } from '@vue/runtime-core'
+
+export default {
+  name: 'Son',
+  // 子组件接收父组件数据使用props属性即可
+  props: {
+    money: {
+      type: Number,
+      default: 0,
+    },
+  },
+  //  setup函数的参数可以接收props属性
+  setup(props) {
+    // 获取父组件数据money
+    // 获取成功
+    console.log(props.money)
+  },
+}
+</script>
+
+<style></style>
